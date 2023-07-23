b0e3a410aebd55e1bd729c13993573cb11a3244a
FloweryWisdom <srgerardoflorenci@gmail.com>
2023-07-23 08:25:56 -0600

    added new folders and files from first week of JS course.

---
+++ Sesion 2: Funciones/main.js
@@ -0,0 +1,95 @@
+// let a = 7;
+// let b = 5;
+// let result = a + b;
+
+// let c = 2;
+// let d = 6;
+// let test2 = c + d;
+
+// let e = 5;
+// let f = 16;
+// let result3 = e + f;
+
+function sumarDosNumeros(a, b) {
+    /*definición de una función*/
+    //   let a=2;
+    //   let b=5;
+    let result = a + b;
+  
+    return result;
+  }
+  
+  let resultadoSuma1 = sumarDosNumeros(2, 5); /*7*/
+  
+  console.log(resultadoSuma1);
+  
+  /*
+      1.- Crear funciones que hagan lo siguiente:
+          sumar dos números
+          restar dos números
+          multiplicar dos números
+          dividir dos números
+      
+      2.- Crear una función que me devuelva el nombre completo de una persona
+  
+      3.- Crear una función que reciba el nombre, color, tamaño, peso y raza de un perro, y me devuelva un objeto que represente a ese perro 
+  */
+  
+  function createDog(name, breed, color) {
+    let ownerName = "Israel";
+    let ownerAddress = "san simón";
+    let dog = { name, breed, color, ownerName, ownerAddress };
+    return dog;
+  }
+  
+  let perroDePrueba = createDog("Otto", "puddle", "blanco");
+  console.log(perroDePrueba);
+  
+  /*
+      1.- crear una función que reciba un nombre y haga lo siguiente:
+          si el nombre tiene más de 5 letras, imprimir en pantalla "el nombre es largo"
+          si no, imprimir "el nombre es corto"
+  */
+  
+  function evaluateNameLength(name) {
+    let nameLength = name.length;
+    let result = nameLength > 5 ? "El nombre es largo" : "El nombre es corto";
+  
+    return result;
+  }
+  
+  let test1 = evaluateNameLength("Israel Salinas Martínez");
+  let test2 = evaluateNameLength("Nao");
+  console.log(test1);
+  console.log(test2);
+  /*
+      2.- Crear una función que reciba 2 números y me devuelva un texto con base en las condiciones siguientes:
+          si el resulta es par, devovler "tu resultado es par"
+          si es non, devolver "el resultado es non"
+  */
+  function evalateAdditionResult(firstNumber, secondNumber) {
+    let additionResult = firstNumber + secondNumber;
+    let result =
+      additionResult % 2 === 0 ? "El resultado es par" : "El resultado es non";
+    return result;
+  }
+  
+  let test3 = evalateAdditionResult(7, 12);
+  console.log(test3);
+  /*
+      3.- Crear una función que reciba el nombre y apellidos de una persona
+          y me devuelva la cantidad de caracteres de el nombre completo 
+  */
+  
+  function getFullNameLength(name, lastname) {
+    let fullName = `Mi nombre es: ${name} ${lastname} y ${evaluateNameLength(
+      name
+    )}`;
+    console.log(fullName);
+    let fullNameLength = fullName.length;
+    return fullNameLength;
+  }
+  
+  name + " " + fullname;
+  
+  getFullNameLength("Israel", "Salinas Martínez");
---
+++ Sesion 3: Arrow Functions & String Methods/main.js
@@ -0,0 +1,133 @@
+function myFunction(arg1, arg2) {
+    let result = arg1 + arg2;
+    return result; /*return explícito*/
+  }
+  
+  const myArrowFunction = (arg1, arg2) => arg1 + arg2;
+  
+  /*
+  const getEvens = (number) => {
+      let result = number % 2 === 0 ? true : false
+      return result
+  }*/
+
+  const getEvens = (number) => number % 2 === 0;
+  
+  console.log(getEvens(17));
+  console.log(getEvens(10));
+  let conventionalResult = myFunction(10, 20);
+  console.log(conventionalResult);
+  
+  let arrowResult = myArrowFunction(10, 20);
+  console.log(arrowResult);
+  
+  let user = {
+    name: "Israel",
+    lastName: "Salinas Martínez",
+    greeting: (personname) =>
+      `Hola ${personname}, soy ${user.name} ${user.lastName}`,
+  };
+  
+  let test1 = user.name;
+  console.log(test1);
+  let test2 = user.greeting("Nefer");
+  console.log(test2);
+  
+  /*
+      Necesito mostrar las iniciales del nombre completo de una persona
+      "Isaac Salinas Martínez"
+      "I. S. M. "
+  */
+  
+  //string.split(separator);
+  
+  let miNombreCompleto = "Israel Salinas Martínez";
+  
+  console.log(miNombreCompleto.split("s"));
+  console.log("Israel".charAt(0));
+  console.log("Israel".charAt(0).concat(". "));
+  
+  console.log(miNombreCompleto.indexOf(" ")); /*prmer espacio en el nombre*/
+  console.log(miNombreCompleto.charAt(miNombreCompleto.indexOf(" ") + 1));
+  console.log(miNombreCompleto.indexOf(" ", 7)); /*segundo espacio en el nombre*/
+  
+  console.log(miNombreCompleto[0]);
+  let names = ["israel", "Salinas"];
+  console.log(names[0]);
+  
+  
+  // Israel, 2 => r
+  // Israel, 10 => el número excede la cantidad de caracteres del string
+  
+  console.log("israel"[10]);
+  
+  // const checkCharAtPosition = (str, position) =>
+  //   str.charAt(position) === ""
+  //     ? "el número excede la cantidad de caracteres del string"
+  //     : str.charAt(position);
+  const checkCharAtPosition = (str, position) => {
+    return str[position]
+      ? str[position]
+      : "el número excede la cantidad de caracteres del string";
+  };
+  
+  //false || / || null || 0 || "" (falsy)
+  
+  //true   truthy => "israel"
+  
+  let testPosition = checkCharAtPosition("Israel", 10);
+  console.log(testPosition);
+  
+  /* recibir dos números, y entregar el resultado de la resta entre ambos (a - b), si los números son iguales, entregar el mensaje "los números son iguales"*/
+  
+    
+
+/* T A R E A */
+
+
+const myString = "The quick brown fox jumps over over the lazy dog.";
+
+
+/* 1. Necesitamos saber de cualquier string, el caracter que se encuentre en cualquier posición que indiquemos. Si la posición excede la cantidad de caracteres, indicar que no es posible ejecutar el ejercicio. */
+
+const findCharacter = (string, index ) => {
+    index >= 0 && index < string.length
+    ? console.log(`Character at index ${index} is: ${string.charAt(index)}`)
+    : console.log(`Error: Index is out of range.`)
+}
+let output = findCharacter(myString, 23)
+let output2 = findCharacter(myString, 75)
+
+      
+/* 2. Necesitamos saber si un texto cualquiera tiene dentro otro string.
+
+        Ejemplo: "Israel" "ra" - si lo incluye
+        Ejemplo: "Israel" "ac" - no lo incluye */
+
+const checkSubstring = (primaryString, userInput) => {
+    primaryString.includes(userInput)
+    ? console.log(`${userInput} is in the string`)
+    : console.log(`${userInput} is not in the string`)
+}
+let userInput = "xr"; checkSubstring(myString, userInput);
+let userInput2 = "fox"; checkSubstring(myString, userInput2)
+
+
+/* 3. Necesito saber la cantidad de palabras de un texto cualquiera. */
+
+const getFullNameLength = string => console.log(`${string.length}`);
+
+let length3 = getFullNameLength(myString) 
+
+
+/* 4. Necesito reemplazar todas las coincidencias de una palabra dentro de un texto, con otro texto.
+
+        Ejemplo: "mi perro es gris y es un latoso" "es" "no es"
+        Ejemplo:"mi perro no es gris y no es un latoso" */
+const replaceSubstring = (string, target, replacement) =>
+    string.replace(new RegExp(target, "gi"), replacement);
+
+let originalString = "Flowery is a person and is awesome";
+let newString = replaceSubstring(originalString, "is", "is not");
+
+console.log(newString);
